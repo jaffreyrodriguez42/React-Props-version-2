1b829aeb7cc7b1ef43783cd82cca01770557d7f6
Jaff <jaffreyrodriguez42@gmail.com>
2021-02-25 00:34:37 +0800

    React props practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts";
+
+console.log(contacts);
 
 function App(){
 	return(
 	<div>
-		<Card name="Beyonce" 
-			  imgSrc="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
-			  imgAlt="avatar_img" 
-			  tel="+123 456 789" 
-			  email="b@beyonce.com"/>
-		<Card name="Donovan Mitchell" 
-			  imgSrc="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" 
-			  imgAlt="avatar_img" 
-			  tel="+987 654 321" 
-			  email="jack@nowhere.com" img/>
-		<Card name="Jaff Rodriguez" 
-			  imgSrc="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" 
-			  imgAlt="avatar_img" 
-			  tel="+918 372 574" 
-			  email="gmail@chucknorris.com" />
+		<Card name={contacts[0]["name"]} 
+			  imgSrc={contacts[0]["imgURL"]} 
+			  tel={contacts[0]["phone"]} 
+			  email={contacts[0]["email"]}/>
+	  	<Card name={contacts[1]["name"]} 
+	  	  	  imgSrc={contacts[1]["imgURL"]} 
+	  	      tel={contacts[1]["phone"]} 
+	  	      email={contacts[1]["email"]}/>
+	  	<Card name={contacts[2]["name"]} 
+	  	  	  imgSrc={contacts[2]["imgURL"]} 
+	  	  	  tel={contacts[2]["phone"]} 
+	  	  	  email={contacts[2]["email"]}/>
 	</div>
 	);
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,21 @@ import React from "react";
 function Card(props){
 	return(
 		<div>
-			<h1>My Contacts</h1>
-
-			<h2>{props.name}</h2>
-			<img
-			  src={props.imgSrc}
-			  alt={props.imgAlt}
-			/>
-			<p>{props.tel}</p>
-			<p>{props.email}</p>
-		</div>
+		      <h1 className="heading">My Contacts</h1>
+		      <div className="card">
+		        <div className="top">
+		          <h2 className="name">{props.name}</h2>
+		          <img className="circle-img"
+		            src={props.imgSrc}
+		            alt="avatar_img"
+		          />
+		        </div>
+		        <div className="bottom">
+		          <p className="info">{props.tel}</p>
+		          <p className="info">{props.email}</p>
+		        </div>
+		      </div>
+		    </div>
 	);
 }
 
